b53734cecb09c67ff51f402c0b9aeeb4531fd1ec
teddy2021 <darkking@live.ca>
2021-09-30 23:32:00 -0400

    Debugged the prediction algorithm and began debuggin the update rule for GD.

--- Assignment1/cps803_Assignment1/src/featuremaps/featuremap.py
+++ Assignment1/cps803_Assignment1/src/featuremaps/featuremap.py
@@ -2,6 +2,8 @@ import util
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import random
+import pdb
 np.seterr(all='raise')
 
 factor = 2.0
@@ -29,12 +31,12 @@ class LinearModel(object):
         # *** START CODE HERE ***
         m = []
         for i in range(0, len(X)):
-            vec = X[i][0]
+            vec = X[i]
             m.append(vec)
         matrix = np.array(m)
 
         multd = np.matmul(np.transpose(matrix), matrix)
-        ident = np.identity(np.shape(multd)[0])
+        ident = np.identity(multd.shape[0])
         res = np.linalg.solve(multd, ident)
 
         multd = np.matmul(np.transpose(matrix), y)
@@ -55,13 +57,16 @@ class LinearModel(object):
 
         res = self.predict(X)
 
-        step = alpha * np.sum(res[:] - y[:])
-        for i in range(0, len(res)):
-            vec = X[i]
-            for j in range(0, len(self.theta[0])):
-                val = self.theta[0][j]
-                val -= step * vec[j]
-                self.theta[0][j] = val
+        update = np.zeros(np.shape(X)[1],dtype='float')
+
+        for i in range(0, len(X)):
+            for j in range(0,update.size):
+                update[j] += alpha *(  res[i] - y[i] ) * X[i][j]
+
+        for i in range(0, self.theta.size - 1):
+            self.theta[0][i] -= update[i]
+
+
         # *** END CODE HERE ***
 
 
@@ -76,15 +81,13 @@ class LinearModel(object):
         # *** START CODE HERE ***
         alpha = 0.01
 
-        lst = []
         for i in range(0, len(X)):
-            lst.append( self.predict(X[i]))
-            res = np.array(lst)
             vec = X[i]
-            step = alpha * (np.sum(res[:] - y[:len(res) - 1]))
-            for j in range(0, len(self.theta)):
-                val = self.theta[0][i]
-                val -= step * vec[j]
+            res = self.predict(vec)
+            for j in range(0, self.theta.size):
+                val = self.theta[0][j]
+                step = alpha * (res - y[i]) * X[i][j]
+                val -= step
                 self.theta[0][j] = val
         # *** END CODE HERE ***
 
@@ -103,11 +106,9 @@ class LinearModel(object):
         res = []
         for x in X:
             val = x[1]
-            i = 0
             lst = []
-            while i <= k:
+            for i in range(0, k + 1):
                 lst.append(val ** i)
-                i += 1
             res.append(np.array(lst))
         return np.array(res)
         # *** END CODE HERE ***
@@ -149,11 +150,14 @@ class LinearModel(object):
         # *** START CODE HERE ***
         if self.theta is None:
             return None
+        if np.ndim(X) == 1:
+            return self.theta.dot(X)
+
         results = []
         for x in X:
-            results.append(self.theta.T[0].dot(x.T[0]))
-        return np.array(results).reshape((len(results), ))
-        # *** END CODE HERE ***
+            results.append(self.theta.dot(x))
+        return np.array(results).reshape(len(results), )
+        # *** END CODE HERE **ee if array has specific shape
 
 
 def run_exp(train_path, cosine=False, ks=[1, 2, 3, 5, 10, 20], filename='plot.pdf'):
@@ -162,6 +166,10 @@ def run_exp(train_path, cosine=False, ks=[1, 2, 3, 5, 10, 20], filename='plot.pd
     plot_x = np.ones([1000, 2])
     plot_x[:, 1] = np.linspace(-0.1, 1.1, 1000)
     plt.figure()
+    for i in range(0, len(train_x)):
+        train_x[i][1] = (i/2) + random.random()
+        train_y[i] = i
+
     plt.scatter(train_x[:, 1], train_y)
 
     for k in ks:
@@ -169,12 +177,12 @@ def run_exp(train_path, cosine=False, ks=[1, 2, 3, 5, 10, 20], filename='plot.pd
         Our objective is to train models and perform predictions on plot_x data
         '''
         # *** START CODE HERE ***
-        model = LinearModel(np.zeros([1,4]))
+
+        model = LinearModel(0.5*np.zeros([1,4]))
         x = model.create_poly(3, train_x)
-        exp = k
-        if exp > 3:
-            exp = 3
-        print("10^", exp + 1, "iterations")
+        if filename == "MedLinGD.pdf":
+            print(x)
+        exp = 1
 
         for i in range(0, (100*(10**(exp)))):
             model.fit_GD(x, train_y)
@@ -200,8 +208,8 @@ def main(medium_path, small_path):
     Run all expetriments
     '''
     # *** START CODE HERE ***
-    run_exp(small_path, ks=[1,2,3], filename="SmallGD.pdf")
-    run_exp(medium_path, ks=[1,2,3], filename="MediumGD.pdf")
+    run_exp(small_path, ks=[1,2,3], filename="SmallLinGD.pdf")
+    run_exp(medium_path, ks=[1,2,3], filename="MedLinGD.pdf")
     # *** END CODE HERE ***
 
 
