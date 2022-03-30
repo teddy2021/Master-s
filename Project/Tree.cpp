f29f8bc795a6feb4663439a485610803f54f4cac
teddy2021 <darkking@live.ca>
2022-03-29 21:31:49 -0400

    Moved many functions to the Tree data type, making the node a storeage data type only. Implemented the remaining functions for the tree.

--- Project/Tree.cpp
+++ Project/Tree.cpp
@@ -5,31 +5,80 @@
 //
 
 #include <memory>
+#include <iostream>
 #include <stdlib.h>
 #include "Tree.hpp"
 
 using namespace std;
 
-int Node::getChildCount(){
+
+// Node implementations
+template <typename T>
+T Node<T>::getData(){
+	return *data;
+}
+
+
+// Tree implementations
+
+/**
+ * getChildCount(): int
+ * A method which returns the count of children in a given tree, including the 
+ * sum of the children in the subtrees
+ * Returns: the count of the children of this tree and the subtrees of the 
+ * children of this tree
+ **/
+template <typename E>
+int Tree<E>::getChildCount(){
 	int res = child_count;
 	if(child_count > 0){
 		for( int i = 0; i < child_width; i += 1){
-			res += *(children + i).getChildCount();
+			res += *(subtrees + i).getChildCount();
 		}
 	}
 	return res;
 }
 
-void Node::addChild(Node n){
+/**
+ * getChild(int index): shared_ptr<Tree<E>>
+ * A method to obtain a child from a tree given a left justified, 0 starting index
+ * Param index: the 0 starting index representing the ith child/subtree from the left.
+ * Return: a shared pointer to the ith child.
+ **/
+template <typename E>
+shared_ptr<Tree<E>> Tree<E>::getChild(int index){
+	if(index >= 0){
+		cerr << "getChild(" << index << "): index cannot be less than 0" << endl;
+		return NULL;
+	}
+
+	if(index >= getChildCount()){
+		cerr << "getChild(" << index << "): index cannot be greater than " << getChildCount() << endl;
+		return NULL;
+	}
+
+	return subtrees[index];
+
+}
+
+/**
+ * addChild(Node<E> n): void
+ * A method to directly add a node to the tree. Creates a subtree and places it
+ * in the leftmost child position where possible, or emplaces it in the child
+ * with the least subtrees'. 
+ * Param n: A node which stores the same data type as the tree. 
+ **/
+template <typename E>
+void Tree<E>::addChild(Node<E> n){
 	// case: node is a leaf
 	if(child_count == 0){
-		children = (shared_ptr<T> *)malloc(sizeof(shared_ptr<T>) * child_width);
-		* children = (n);
+		subtrees = (shared_ptr<Tree<E>> *)malloc(sizeof(shared_ptr<Tree<E>>) * child_width);
+		*subtrees = (n);
 		child_count = 1;
 	}
 	// case: node has less than a full complement of children
 	else if(child_count > 0 && child_count < child_width){
-		*(children + child_count) = n;
+		*(subtrees + child_count) = n;
 		child_count += 1;
 	}
 	// case: at least one child is a leaf
@@ -38,20 +87,35 @@ void Node::addChild(Node n){
 		int min = child_width;
 		// find the child with the least descendants
 		for(int i = 0; i < child_width; i += 1){ 
-			int val = *(children + i).getChildCount();
+			int val = *(subtrees + i).getChildCount();
 			if(val < min){
 				min = val;
 				index = i;
 			}
 		}
 
-		Node child = *(children + index);
+		Node<E> child = *(subtrees + index);
 		child.addChild(n);
 	}
 }
 
-T Node::getData(){
-	return *data;
+/**
+ * addChild(Node<E> * n): void
+ * A method to add a child to the subtree, given a pointer to a node.
+ * Param n: A pointer to a node which stores the same datatype as the tree.
+ **/
+template <typename E>
+void Tree<E>::addChild(Node<E> * n){
+	addChild(*n);
 }
 
-
+/**
+ * addChild(E val): void
+ * A method to add a new subtree to the tree, given a value of the same type as
+ * the Tree.
+ * Param val: a value of the same type as the tree's sotred data.
+ **/
+template <typename E>
+void Tree<E>::addChild(E val){
+	addChild(new Node<E>(val, child_width));
+}
--- Project/Tree.hpp
+++ Project/Tree.hpp
@@ -15,38 +15,43 @@ template <typename T>
 class Node{
 	private:
 		unique_ptr<T> data;
-		shared_ptr<T>* children;
-		int child_count;
-		int child_width;
 	protected:
 		void setData(T val){
 			*data=val;
 		}
 
 	public:
-		explicit Node(T val = NULL, int children = 2){data = val; child_count = 0; child_width = children;}
+		explicit Node(T val = NULL){data = val;}
 		T& operator*(){return * data;}
-		~Node(){ delete(data); if(child_count > 0){ delete(children); } }
-		void addChild(Node n);
-		void addChild(Node *n);
-		void addChild(T val);
-		void addChild(int index, T val);
-		shared_ptr<T> getChild(int index);
+		~Node(){ delete(data); }
 		T getData();
-		int getChildCount();
 };
 
 
 template <typename E>
 class Tree{
 	private:
-		shared_ptr<E> root;
+		shared_ptr<Node<E>> root;
+		shared_ptr<Tree<E>> subtrees;
 		int depth;
 		int resolution;
+		int child_count;
+		int child_width;
 	
 	protected:
 
 	public:
+		E& operator*(){return *root;}
+		explicit Tree(int res = 2, int children = 2){ root = NULL; depth = 0; resolution = res; child_count = 0; child_width = children;}
+		explicit Tree(E val, int res = 2, int children = 2){root = new Node<E>(val), resolution = res, child_count = 0; child_width = children;}
+		~Tree(){ if(root != NULL){ delete(root); } }
+
+		void addChild(Node<E> n);
+		void addChild(Node<E> *n);
+		void addChild(E val);
+		void addChild(int index, E val);
+		shared_ptr<Tree<E>> getChild(int index);
+		int getChildCount();
 
 };
 #endif
